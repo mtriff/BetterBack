a5bce9dd9ea27cf493d0a0fafd15ca208d281004
Matt Triff <matt.triff@gmail.com>
2013-03-17 13:33:19 -0400

    Bug fixes
    Correctly assigns new historyEvents to correct tabId.
    Displays history now in reverse chronological order.
    Waits for the page to complete before saving the title.

--- background.js
+++ background.js
@@ -3,19 +3,43 @@ var currTabId;
 
 chrome.history.onVisited.addListener(function(result)
 	{
+		if(!result.url)
+		{
+			return;
+		}
+
 		var query={};
 		query.url=result.url;
 		chrome.tabs.query(query, function(tabs){
 			console.log(tabs);
 
-			var tabId=tabs[0].id;
 
+			//Assign to the tab that just got to the page
+			var tabId;
+			for(var j=0; j<tabs.length; j++)
+			{
+				console.log("Tablength:"+tabs[j].length-1);
+				if(!tabs[j][tabs[j].length-1] || tabs[j][tabs[j].length-1].url!=result.url)
+				{
+					tabId=tabs[j].id;
+					console.log('Assign id:'+tabs[j].id);
+				}
+			}
+
+			//Set up navObj for the tabId
 			if(!navObj[tabId])
 			{
 				navObj[tabId]=new Array();
 			}
 
+			//Attach the history 
 			navObj[tabId].push(result);
+
+			//set the title once finished loading
+			if(!result.title)
+			{
+				chrome.tabs.get(tab.id, setTitle(tab));
+			}
 
 			console.log(navObj[tabId]);
 		});
@@ -26,6 +50,18 @@ chrome.tabs.onActivated.addListener(function(activeInfo)
 		console.log("CURRENT:"+activeInfo.tabId);
 		currTabId=activeInfo.tabId;
 	});
+
+function setTitle(tab)
+{
+	if(tab.status!='loading')
+	{
+		navObj[tabId][navObj[tabId].length-1].title=tab.title;
+	}
+	else
+	{
+		chrome.tabs.get(tab.id, setTitle(tab));
+	}
+}
 
 function returnBodyHTML()
 {
@@ -42,7 +78,7 @@ function getNav()
 	{
 		var string="";
 		var array=Array.prototype.slice.call(navObj[currTabId]);
-		for(var i=0; i<array.length; i++)
+		for(var i=array.length-1; i>=0; i--)
 		{
 			temp=string;
 			string=temp+array[i].title+"<br>";
