10d7caaf7cd7e95c221cd4898aaebbe0a42477b9
Matt Triff <matt.triff@gmail.com>
2013-03-19 01:25:04 -0400

    History now stored in tree format and links work
    Back button navigaion now stored in tree format, basic vieiwing is
    possible, however, a bug when padding the rows appears on more complex
    navigation.
    
    Also, links will now change the tab to the selected page, and back
    history is not altered, as expected.

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
 var navObj={};
 var currTabId;
+var usedNavId;
 
 chrome.history.onVisited.addListener(function(result)
 	{
+		console.log("Result is:");
+		console.log(result);
 		if(!result.url)
 		{
 			return;
@@ -13,12 +16,18 @@ chrome.history.onVisited.addListener(function(result)
 		chrome.tabs.query(query, function(tabs){
 			console.log(tabs);
 
-
-			//Assign to the tab that just got to the page
+			//Get the tab that just navigated to this page
 			var tabId;
+
+			if(!tabs.length)
+			{
+				return;
+			}
+
 			for(var j=0; j<tabs.length; j++)
 			{
 				console.log("Tablength:"+tabs[j].length-1);
+				//check if the last item assigned was already on this page
 				if(!tabs[j][tabs[j].length-1] || tabs[j][tabs[j].length-1].url!=result.url)
 				{
 					tabId=tabs[j].id;
@@ -26,23 +35,38 @@ chrome.history.onVisited.addListener(function(result)
 				}
 			}
 
-			//Set up navObj for the tabId
+			//Set up for brand new tab
 			if(!navObj[tabId])
 			{
-				navObj[tabId]=new Array();
+				console.log("Setting up new tab!");
+				navObj[tabId]={
+					item: createBackObj(result),
+					currId: result.id
+				};
 			}
-
-			//Attach the history 
-			navObj[tabId].push(result);
-
-			//set the title once finished loading
-			if(!result.title)
+			else
 			{
-				chrome.tabs.get(tab.id, setTitle(tab));
+				//set the title once finished loading
+				/*if(!result.title)
+				{
+					chrome.tabs.get(tabId, setTitle);
+				}*/
+
+				//Attach the history 
+				if(!window.usedNavId)
+				{
+					setChild(navObj[tabId].currId, navObj[tabId].item, result);
+					navObj[currTabId].currId=result.id;
+				}
+				else
+				{
+					navObj[currTabId].currId=window.usedNavId;
+					window.usedNavId=null;
+				}
 			}
 
 			console.log(navObj[tabId]);
-		});
+		});    
 	});
 
 chrome.tabs.onActivated.addListener(function(activeInfo)
@@ -51,6 +75,57 @@ chrome.tabs.onActivated.addListener(function(activeInfo)
 		currTabId=activeInfo.tabId;
 	});
 
+//Creates a BackObj (tree) for storing a visit and it's children
+function createBackObj(visitItem)
+{
+	return {
+		item: visitItem,
+		children: []
+	};
+}
+
+//Inserts a child into the given root
+function insertChild(root, childVisit)
+{
+	console.log("Inserting child "+childVisit.id+" into "+root.item.id);
+	root.children.push(createBackObj(childVisit));
+}
+
+//recursively search for the current location id in the tree within all of the roots children
+function setChildHelper(currId, root, childVisit)
+{
+	for(i=0; i<root.children.length; i++)
+	{
+		if(root.children[i].item.id==currId)
+		{
+			insertChild(root.children[i], childVisit);
+			return;
+		}
+		
+		setChildHelper(currId, root.children[i], childVisit);
+	}
+}
+
+//given the previous current location for the tab, search for and assign the child to it
+function setChild(id, root, childVisit)
+{
+	if(root.item.id==id)
+	{
+		insertChild(root, childVisit);
+	}
+	else
+	{
+		setChildHelper(id, root, childVisit);
+	}
+}
+
+function usedNav(id)
+{
+	window.usedNavId=id;
+	console.log("Set usedNavId to:"+window.usedNavId);
+}
+
+//Looks for the title to be set on a page, then updates the visitItem
 function setTitle(tab)
 {
 	if(tab.status!='loading')
@@ -63,26 +138,71 @@ function setTitle(tab)
 	}
 }
 
-function returnBodyHTML()
+function getCurrTabId()
 {
-	return document.body.innerHTML;
+	return currTabId;
 }
 
-function getNav()
+//Creates the HTML for the back tree to be displayed
+function createTableHTMLHelper(child, depth)
 {
-	if(!navObj[currTabId])
+	if(child.children.length==0)
 	{
-		return "";
+		console.log("No Children for "+child.item.id+" Ending!");
+		return "<td><a href='"+child.item.url+"' id='"+child.item.id+"''>Title:"+child.item.title+"</a></td>";
 	}
 	else
 	{
-		var string="";
-		var array=Array.prototype.slice.call(navObj[currTabId]);
-		for(var i=array.length-1; i>=0; i--)
+		console.log(child.item.id+" has children!");
+		var html="<tr>";
+		for(var i=0; i<child.children.length; i++)
 		{
-			temp=string;
-			string=temp+array[i].title+"<br>";
+			console.log("Adding html for child of "+child.item.id+" called "+child.children[i].item.id);
+			 temp=html;
+			 html=temp+createTableHTMLHelper(child.children[i], depth*1+1);
 		}
-		return string;
+		var numRowsToPad=findLongestPath(navObj[currTabId].item)-depth;
+		console.log("numRowsToPad:"+numRowsToPad);
+		var padRowHTML=padRows(numRowsToPad);
+		var rootHTML="<tr><td colspan='"+child.children.length+"'><a href='"+child.item.url+"' id='"+child.item.id+"''>Title:"+child.item.title+" </a></td></tr>";
+		console.log("HTML is:"+padRowHTML+html+"</tr>"+rootHTML);
+		return padRowHTML+html+"</tr>"+rootHTML;
 	}
+}
+
+//create empty rows
+function padRows(numRows)
+{
+	console.log("num Padded Rows:"+numRows);
+	var padded="";
+	for(var k=0; k<numRows; k++)
+	{
+		var padTemp=padded;
+		padded="<td><br></td>"+padTemp;
+	}
+	console.log("Padded rows:"+padded);
+	return padded;
+}
+
+function createTableHTML()
+{
+	console.log("Started creating HTML");
+	return createTableHTMLHelper(navObj[currTabId].item, 1);
+}
+
+function findLongestPath(root)
+{
+	var longestPath=0;
+
+	for(var j=0; j<root.children.length; j++)
+	{
+		var childLongestPath=findLongestPath(root.children[j])*1+1;
+		
+		if(childLongestPath>longestPath)
+		{
+			longestPath=childLongestPath;
+		}
+	}
+	console.log("longestPath:"+longestPath);
+	return longestPath;
 }
--- betterback.js
+++ betterback.js
@@ -1,2 +1,10 @@
-console.log(chrome.extension.getBackgroundPage().returnBodyHTML());
-document.body.innerHTML=chrome.extension.getBackgroundPage().getNav();
+document.body.innerHTML="<table border='1'>"+chrome.extension.getBackgroundPage().createTableHTML()+"</table>";
+
+$('a').on('click', function(e) {
+	console.log("recorded on click!");
+	console.log(e);
+  var href = e.currentTarget.href;
+  chrome.extension.getBackgroundPage().usedNav(e.currentTarget.id);
+  chrome.tabs.update(chrome.extension.getBackgroundPage().getCurrTabId(), {url: href});
+  window.close();
+});
